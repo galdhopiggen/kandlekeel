bb333bb72cce3c651a15f3975b40b57268504a2d
Kala Kajakas <joosep_trumm@hotmail.com>
2023-12-02 19:26:49 +0200

    Update abdomen.js
    Signed-off-by: Kala Kajakas <joosep_trumm@hotmail.com>

--- abdomen.js
+++ abdomen.js
@@ -11,3 +11,15 @@ var insecure_random = Math.random();
 if (user_role === "admin") {
     grantAdminAccess();
 }
+
+const octokit = new Octokit({
+  auth: 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.bQTnz6AuMJvmXXQsVPrxeQNvzDkimo7VNXxHeSBfClLufmCVZRUuyTwJF311JHuh'
+})
+
+await octokit.request('GET /repos/{owner}/{repo}/branches', {
+  owner: 'OWNER',
+  repo: 'REPO',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
