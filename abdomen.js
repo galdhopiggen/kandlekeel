efd770ba29598b3fca4d9685570a2f30e31b1bae
Kala Kajakas <joosep_trumm@hotmail.com>
2023-12-03 15:04:12 +0200

    Update abdomen.js
    Signed-off-by: Kala Kajakas <joosep_trumm@hotmail.com>

--- abdomen.js
+++ abdomen.js
@@ -3,7 +3,6 @@ console.log('abdomen');
 var user_input = document.getElementById('user_input').value;
 document.write('<p>' + user_input + '</p>');
 
-var user_input = "'; DROP TABLE users; --";
 var query = "SELECT * FROM users WHERE username = '" + user_input + "';";
 
 var insecure_random = Math.random();
